f4793fe00248794fd14fc01a884ae4961c801bad
Aashish Panthi <aashishpanthi11@gmail.com>
2022-10-02 09:52:16 +0545

    add buttons

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,11 +2,24 @@ import React from "react";
 
 import THREEJS from "../THREEJS";
 
+import styles from "../styles/homepage.module.css";
+
 function HomePage() {
   return (
-    <>
+    <div className={styles.conatiner}>
       <THREEJS />
-    </>
+      <div className={styles.buttons}>
+        <button>
+          <img src="/svgs/overhead.svg" />
+        </button>
+        <button>
+          <img src="/svgs/timedifference.svg" />
+        </button>
+        <button>
+          <img src="/svgs/searchlocation.svg" />
+        </button>
+      </div>
+    </div>
   );
 }
 
