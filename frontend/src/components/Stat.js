e0b7754bf01fc3eeb322082c2f5555e19b865276
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2022-10-02 18:06:28 +0545

    configure

--- frontend/src/components/Stat.js
+++ frontend/src/components/Stat.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/stat.module.css";
 import axios from "axios";
 
@@ -7,12 +7,32 @@ const Stat = () => {
   const [lon, setLon] = useState(0);
   const [issVel, setIssvel] = useState(0);
   const [repeater, setRepeater] = useState(0);
+
+  const color = [
+    "#FF6633",
+    "#FFB399",
+    "#FF33FF",
+    "#FFFF99",
+    "#00B3E6",
+    "#E6B333",
+    "#3366E6",
+    "#999966",
+    "#99FF99",
+    "#B34D4D",
+    "#80B300",
+    "#809900",
+    "#E6B3B3",
+    "#6680B3",
+    "#66991A",
+  ];
   const getLocation = async () => {
-    const {data} = await axios.get("https://api.wheretheiss.at/v1/satellites/25544");
+    const { data } = await axios.get(
+      "https://api.wheretheiss.at/v1/satellites/25544"
+    );
     setLat(data.latitude);
     setLon(data.longitude);
-    setIssvel(data.velocity);
-  }
+    setIssvel(data.velocity.toString().substring(0, 8));
+  };
   useEffect(() => {
     getLocation();
     setTimeout(() => {
