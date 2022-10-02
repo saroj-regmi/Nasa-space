a2e6f7845ed2960b90e564ffd354430c1eeeba83
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2022-10-02 16:32:19 +0545

    stat api done

--- frontend/src/components/Stat.js
+++ frontend/src/components/Stat.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import styles from "../styles/stat.module.css";
+import axios from "axios";
 
 const Stat = () => {
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  const [issVel, setIssvel] = useState(0);
+  const [repeater, setRepeater] = useState(0);
+  const getLocation = async () => {
+    const {data} = await axios.get("https://api.wheretheiss.at/v1/satellites/25544");
+    setLat(data.latitude);
+    setLon(data.longitude);
+    setIssvel(data.velocity);
+  }
+  useEffect(() => {
+    getLocation();
+    setTimeout(() => {
+      setRepeater((prevState) => prevState + 1);
+    }, 5000);
+  }, [repeater]);
+
   return (
     <>
       <div className={styles.stat_container}>
         <p>
-          Latitude: <span>20N 56,30E 44</span>
+          Latitude: <span>{lat}</span>
         </p>
         <p>
-          Longitude: <span>50N 57,45E 67</span>
+          Longitude: <span>{lon}</span>
         </p>
         <p>
-          Earth Velcotiy: <span>10mls</span>
+          Earth Velcotiy: <span>30 km/hr</span>
         </p>
         <p>
-          ISA velocity: <span>160m/s</span>
+          ISA velocity: <span>{issVel} km/hr</span>
         </p>
       </div>
     </>
